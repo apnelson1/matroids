24a8b7639e043a62360f715e0fa0d01c6a75fa08
Peter Nelson <apnelson@uwaterloo.ca>
2022-06-15 16:22:10 -0400

    no precompl needed

--- src/supermatroid/dual'.lean
+++ src/supermatroid/dual'.lean
@@ -4,97 +4,100 @@ import .weak_compl
 
 universes u v
 
+/-
+lemma basis.exists_extension_from (hb : M.basis b) (x : α) : 
+  ∃ b', b' ≤ x ⊔ b ∧ M.basis b' ∧ (M.basis_of (b' ⊓ x) x) :=
+
+  M.basis_of i x ↔ i ≤ x ∧ 
+    ∃ b, B b ∧ i ≤ b ∧ ∀ b', B b' → i ≤ b' ⊓ x → b' ⊓ x = i
+
+  M.basis_of (b ⊓ x) x ↔ 
+    ∀ b', B b' → b ⊓ x ≤ b' → b ⊓ x = b' ⊓ x
+-/
+
+
+open set 
+
+-- lemma bar {α : Type u} {i x : α} [partial_order α] (B : α → Prop) (h_ne : ∃ b, B b) 
+--   (h_ac : is_antichain (≤) B) (h_mid : middle B) (h_ml : max_lower B) :
+--   ∃ j, i ≤ j ∧ M.basis_of j x := 
+-- begin
+--   obtain ⟨b,hb,hbi⟩ := hi, 
+--   obtain ⟨b',⟨hb',hb'x,hb'_max⟩⟩ := hb.max_inf x, 
+--   refine ⟨b' ⊓ x, le_inf ((le_inf hbi hix).trans hb'x) hix, hb'.inf_right_indep _,inf_le_right, _⟩, 
+--   rintros j ⟨b'',hb'',hjb''⟩ hjx hb'j, 
+--   have h₀ := (le_inf hjb'' hjx), 
+--   rw [←(hb'_max _ hb'' (hb'j.trans hjb''))] at h₀, 
+--   exact le_antisymm hb'j h₀, 
+-- end 
 
 namespace supermatroid
 
-variables {α : Type u} [lattice α] 
+--example 
 
-section lattice_dual 
+
+
+variables {α : Type u} [lattice α] [is_modular_lattice α]
+
+lemma foo (M : supermatroid α) : min_upper M.basis := 
+begin
+  rintros x y ⟨s,⟨b,hb,hbs⟩,hxs,hsy⟩, 
+  obtain ⟨b₁,hb₁b,hb₁,hb₁x⟩ := hb.inf_basis_of x, 
+  refine ⟨x ⊔ b₁,⟨⟨b₁,hb₁,le_sup_right⟩,le_sup_left,sup_le (hxs.trans hsy) 
+    (hb₁b.trans ((sup_le hxs hbs).trans hsy))⟩, _⟩, 
+  
+  rintros t ⟨⟨b₂,hb₂,hb₂t⟩,hxt,hty⟩ htx,  
+  refine le_antisymm (sup_le hxt _) htx,
+  by_contradiction hb₁t, 
+  set j := b₁ ⊓ (x ⊔ b₂) with hj, 
+
+  have hj_lt : j < b₁ := inf_le_left.lt_of_ne 
+    (λ h_eq, hb₁t (by {rw ← h_eq, exact inf_le_right.trans (sup_le hxt hb₂t)})), 
+
+  obtain ⟨b₃, hb₃, hjb₃, hb₃j⟩ := hb₁.lt_basis_sup hj_lt hb₂, 
+  
+  have h1 := @inf_lt_inf_of_lt_of_sup_le_sup _ _ _ _ _ x hjb₃ (sup_le_sup_of_le_sup _), 
+  swap, 
+  { rw [hj, inf_comm, inf_sup_assoc_of_le b₁ le_sup_right, le_inf_iff] at hb₃j, 
+    rw [hj, inf_comm, inf_sup_assoc_of_le b₁ le_sup_left, le_inf_iff], 
+    refine ⟨hb₃j.1, hb₃j.2.trans (sup_le le_sup_left (hb₂t.trans _))⟩,
+    rwa sup_comm},
+  refine (hb₁x.not_indep_of_lt (lt_of_le_of_lt (inf_le_inf_of_inf_le _) h1) 
+    inf_le_right (hb₃.inf_right_indep x)), 
+  rw [hj, inf_comm, @inf_comm _ _ _ (x ⊔ b₂)], 
+  exact inf_le_inf_of_inf_le (inf_le_left.trans le_sup_left),  
+end 
+
+
 
 variable [is_modular_lattice α]
 
-lemma middle_dual (M : supermatroid α) : @satisfies_middle_axiom αᵒᵈ _ M.basis :=
-begin
-    rintros x y b hb b' hb' hxy hxb hb'y,
-    obtain ⟨b₀,hb₀,hyb₀,hb₀x⟩ := M.basis_middle y x b' hb' b hb hxy hb'y hxb, 
-    exact ⟨b₀, hb₀, hb₀x, hyb₀⟩, 
-end
 
--- relies on inf_basis_of and lt_basis_sup
---lemma max_inf_dual (M : supermatroid α) : @max_inf αᵒᵈ _ M.basis := M.basis_min_sup
+-- section matroid_dual 
 
-lemma min_sup_dual (M : supermatroid α) : @min_sup αᵒᵈ _ M.basis := M.basis_max_inf
+-- variable [has_precompl α]
 
-lemma min_sup_compl [has_precompl α] (M : supermatroid α) : min_sup (has_precompl.pcompl ⁻¹' M.basis) 
-  :=
-begin
-  intros x b hb, 
-  obtain ⟨b',hb', h1,h2⟩ := M.basis_max_inf xᵒ bᵒ hb, 
-  refine ⟨b'ᵒ, sorry, _,_⟩ ,
-end 
+-- lemma min_sup_of_max_inf_precompl (B : α → Prop) (h : max_inf B) (h' : @max_inf αᵒᵈ _ B) : 
+--   min_sup B := 
 -- begin
---   show ∀ (x b: α) (hb : M.basis b), ∃ (b' : α) (hb': M.basis b'),
---      b ⊓ x ≤ b' ∧ ∀ (b'' : α) (hb'' : M.basis b''), b' ⊓ x ≤ b'' → b'' ⊓ x = b' ⊓ x, 
-
 --   intros x b hb, 
---   obtain ⟨b₁, hxb₁, hb₁, hxxb₁⟩ := hb.sup_super_of x, 
---   refine ⟨b₁,hb₁, by rwa inf_comm at hxb₁, 
---     λ b₂ hb₂ hb₁b₂, inf_eq_inf_of_le_of_le (by_contra (λ hcon, _)) hb₁b₂⟩, 
-  
---   set s := (x ⊓ b₂) ⊔ b₁ with hs_def, 
+--   set y := (order_dual.to_dual x) with hy, 
+--   obtain ⟨b',hb',hb'b,hb'_max⟩ := h' y b hb, 
+--   --have := hb'_max b hb, 
+--   refine ⟨b',hb', _, _⟩, 
 
---   have hb₁s : b₁ < s := lt_of_le_of_ne le_sup_right 
---     (λ h_eq, hcon (by rwa [h_eq, eq_comm, sup_eq_right, inf_comm, ←h_eq] at hs_def)), 
-
---   obtain ⟨b₃, hb₃, hb₃s, hsb₃⟩ := hb₁.basis_inf_lt hb₁s hb₂, 
---   have h₁ := sup_lt_sup_of_lt_of_inf_le_inf hb₃s (_ : s ⊓ x ≤ b₃ ⊓ x), 
---   { refine hxxb₁.not_spanning_of_lt (lt_of_lt_of_le h₁ (sup_le _ le_sup_right)) le_sup_right 
---       (hb₃.sup_right_spanning _), 
---     rw [sup_comm, hs_def],
---     exact sup_le_sup_right inf_le_left _},
-  
---   refine le_inf (le_trans (le_inf inf_le_left _) hsb₃) inf_le_right, 
---   rw [hs_def, sup_inf_assoc_of_le _ (inf_le_left : _ ≤ x), sup_le_iff], 
---   exact ⟨inf_le_right, hb₁b₂⟩, 
 -- end 
 
-/-- The `lattice_dual` of a supermatroid is the supermatroid on the dual order with the same
-  set of bases -/
-def lattice_dual (M : supermatroid α) : supermatroid (αᵒᵈ) :=  
-{ basis := M.basis,
-  exists_basis := M.exists_basis,
-  basis_antichain := M.basis_antichain.to_dual,
-  basis_middle := M.middle_dual,
-  basis_max_inf := M.max_inf_dual,
-  basis_min_sup := M.min_sup_dual, }
-
-end lattice_dual 
-
-section matroid_dual 
-
-variable [has_precompl α]
-
-lemma min_sup_of_max_inf_precompl (B : α → Prop) (h : max_inf B) (h' : @max_inf αᵒᵈ _ B) : 
-  min_sup B := 
-begin
-  intros x b hb, 
-  set y := (order_dual.to_dual xᵒ) with hy, 
-  obtain ⟨b',hb',hb'b,hb'_max⟩ := h' y b hb, 
-  --have := hb'_max b hb, 
-  refine ⟨b',hb', _, _⟩, 
-
-end 
-
-def supermatroid_of_max_inf (B : α → Prop) (h_exists : ∃ b, B b) (h_antichain : is_antichain (≤) B)
-  (h_middle : satisfies_middle_axiom B) (h_max_inf : max_inf B) : supermatroid α := 
-{ basis := B,
-  exists_basis := h_exists,
-  basis_antichain := h_antichain,
-  basis_middle := h_middle,
-  basis_max_inf := h_max_inf,
-  basis_min_sup := sorry  } 
+-- def supermatroid_of_max_inf (B : α → Prop) (h_exists : ∃ b, B b) (h_antichain : is_antichain (≤) B)
+--   (h_middle : satisfies_middle_axiom B) (h_max_inf : max_inf B) : supermatroid α := 
+-- { basis := B,
+--   exists_basis := h_exists,
+--   basis_antichain := h_antichain,
+--   basis_middle := h_middle,
+--   basis_max_inf := h_max_inf,
+--   basis_min_sup := sorry  } 
 
 
-end matroid_dual 
+-- end matroid_dual 
 
 end supermatroid
--- src/supermatroid/easy_lemmas.lean
+++ src/supermatroid/easy_lemmas.lean
@@ -2,14 +2,41 @@ import tactic
 
 universe u
 
-variables {α : Type u} [lattice α] 
+section upper_lower
 
-lemma inf_le_inf_of_inf_le {a b x : α} (h : a ⊓ x ≤ b) : a ⊓ x ≤ b ⊓ x := by simp [h]
+variables {α : Type u} [preorder α]
 
-lemma sup_le_sup_of_le_sup {a b x : α} (h : a ≤ b ⊔ x) : a ⊔ x ≤ b ⊔ x := by simp [h]
+@[reducible] def lower_set_of (s : set α) : set α := {x | ∃ y ∈ s, x ≤ y}
+@[reducible] def upper_set_of (s : set α) : set α := {x | ∃ y ∈ s, y ≤ x}
 
-lemma inf_eq_inf_of_le_of_le {a b x : α} (h1 : a ⊓ x ≤ b) (h2 : b ⊓ x ≤ a) : a ⊓ x = b ⊓ x :=
+lemma upper_set_of_dual (s : set αᵒᵈ) : @upper_set_of αᵒᵈ _ s = @lower_set_of α _ s := rfl 
+lemma lower_set_of_dual (s : set αᵒᵈ) : @lower_set_of αᵒᵈ _ s = @upper_set_of α _ s := rfl  
+
+lemma set.Icc_dual' (x y : α) : @set.Icc αᵒᵈ _ x y = @set.Icc α _ y x := 
+  by {ext, rw [set.mem_Icc, and_comm], refl}
+
+lemma set.Icc_dual'' (x y : α) : @set.Icc αᵒᵈ _ x y = @set.Icc α _ y x := 
+  set.ext (λ x, and_comm _ _)
+
+lemma set.Icc_dual''' (x y : α) : @set.Icc αᵒᵈ _ x y = @set.Icc α _ y x := 
+  set.dual_Icc 
+
+end upper_lower
+
+section lattice
+
+variables {α : Type u} {a b x : α} [lattice α] 
+
+
+
+lemma inf_le_inf_of_inf_le (h : a ⊓ x ≤ b) : a ⊓ x ≤ b ⊓ x := by simp [h]
+
+lemma sup_le_sup_of_le_sup (h : a ≤ b ⊔ x) : a ⊔ x ≤ b ⊔ x := by simp [h]
+
+lemma inf_eq_inf_of_le_of_le (h1 : a ⊓ x ≤ b) (h2 : b ⊓ x ≤ a) : a ⊓ x = b ⊓ x :=
   le_antisymm (inf_le_inf_of_inf_le h1) (inf_le_inf_of_inf_le h2)
 
-lemma sup_eq_sup_of_le_of_le {a b x : α} (h1 : a ≤ b ⊔ x) (h2 : b ≤ a ⊔ x) : a ⊔ x = b ⊔ x := 
+lemma sup_eq_sup_of_le_of_le (h1 : a ≤ b ⊔ x) (h2 : b ≤ a ⊔ x) : a ⊔ x = b ⊔ x := 
   le_antisymm (sup_le_sup_of_le_sup h1) (sup_le_sup_of_le_sup h2)
+
+end lattice 
